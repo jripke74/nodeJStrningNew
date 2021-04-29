0eef9882806a4561557b1fe75aa51348bdaa0e7e
Jeff Ripke <jripke74@gmail.com>
2021-04-29 09:14:18 -0700

    add products array

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
