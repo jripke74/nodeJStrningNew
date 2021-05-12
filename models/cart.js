799d521c18bf6f6f5bf2223995b8b9efacab8bd8
Jeff Ripke <jripke74@gmail.com>
2021-05-12 08:03:46 -0700

    12 add editing logic for products

--- models/cart.js
+++ models/cart.js
@@ -16,13 +16,13 @@ module.exports = class Cart {
       let updatedProduct;
       // Add new product/ increase quantity
       if (existingProduct) {
-        updatedProduct = { ...existingProduct};
+        updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
-        cart.products = [... cart.products]
+        cart.products = [ ...cart.products ]
         cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
-        cart.products = [ ... cart.products, updatedProduct ];
+        cart.products = [ ...cart.products, updatedProduct ];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
       fs.writeFile(p, JSON.stringify(cart), err => {
