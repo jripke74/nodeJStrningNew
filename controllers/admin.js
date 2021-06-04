6aa103c7f557a654a3ae92d5381d1e56d5650172
Jeff Ripke <jripke74@gmail.com>
2021-06-04 10:12:27 -0700

    08 add edit-product route

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,15 +57,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  )
-  product
-    .save()
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save()
+  })
     .then(result => {
       console.log('Updated Product!');
       res.redirect('/admin/products');
@@ -74,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
