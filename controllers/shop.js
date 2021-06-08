8407f1034abc16352a03f3ca8ba43e4056ee7085
Jeff Ripke <jripke74@gmail.com>
2021-06-08 13:45:44 -0700

    19 add logic to display orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -106,16 +106,15 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
-    .catch(err => console.log(err));
+  Order.find({ 'user.userId': req.user._id })
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
