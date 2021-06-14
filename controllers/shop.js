8818209e388a597924c69d8ff9235cd6befc9b8f
Jeff Ripke <jripke74@gmail.com>
2021-06-14 09:14:10 -0700

    18 making add to cart work

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,7 +46,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  req.session.user
+  req.user
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
@@ -65,7 +65,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then(result => {
       console.log(result);
