8f4be8a0857055f6060f4b881d8e849410b5d920
Jeff Ripke <jripke74@gmail.com>
2021-07-28 07:57:00 -0700

    04 add email validation in signup form

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -79,6 +80,14 @@ exports.postSignup = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array(),
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const { check } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -12,7 +13,7 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', check('email').isEmail(), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
